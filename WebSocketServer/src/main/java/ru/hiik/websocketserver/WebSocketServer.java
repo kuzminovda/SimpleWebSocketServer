bd1221531992a8597900defd0776694e0e54b2f2
vaganovdv <vaganovdv@hs>
2020-04-18 15:58:39 +1000

    Добавлена поддержка протокола Websocket

--- WebSocketServer/src/main/java/ru/hiik/websocketserver/WebSocketServer.java
+++ WebSocketServer/src/main/java/ru/hiik/websocketserver/WebSocketServer.java
@@ -22,51 +22,75 @@ import javax.websocket.server.ServerEndpoint;
  *
  * @author vaganovdv
  */
+//  Класс доступен на протяжении всего жизненного цикла приложения
+//   |
+// 
 @ApplicationScoped
+//
+//    Сервер, реализующий протокол Websocket
+//      |
 @ServerEndpoint("/websocket")
-
 public class WebSocketServer
 {
 
     private static final Logger LOG = Logger.getLogger(WebSocketServer.class.getSimpleName());
-    private static ConcurrentMap<String, Session> users = new ConcurrentHashMap<String, Session>();
+    
+    //   Структура для Map (карта) хранения сведений о подключенных пользователях (сесии пользователей)
+    //   
+    //   Пример карты (связанные пары):
+    //        ключ                          значение (экземпляр класса Session)
+    //         |                                            |
+    //      EF91381F-B90C-4593-8886-C0F03C91AE24 - сессия пользователя 1 
+    //      8632E55F-4BB8-468C-89AF-4886E597016B - сессия пользователя 2 
+    //      6CBAB35C-A89E-4952-AB0A-5256ED0279E6 - сессия пользователя 3 
+    //
+    //                              ключ  значение   имя структуры   может использоваться в режиме работы нескольких нитей
+    //                               |      |        |               /
+    private static ConcurrentMap <String, Session> users = new ConcurrentHashMap<String, Session>();
     
     
     
+    // Событие OnOpen возникает в случае подключения к серверу клиента
+    // и создания новой  сессии с уникальным идентификатором
     @OnOpen
-    public void onOpen(Session session) 
+    //                          экземпляр сесси с клиентом Websocket
+    //                            |
+    public void onOpen(Session session)
     {
-       
-       LOG.log(Level.INFO, "Подключено к серверу: "+session.getId());
-        users.putIfAbsent(session.getId().toUpperCase(), session);
+        // Печать идентификатора сесиии
+        LOG.log(Level.INFO, "Подключено к серверу: " + session.getId().toUpperCase());
+        //                     ключ        преобразовать в заглавные   экземпляр класса сессии
+        //                      |           /                          /
+        users.putIfAbsent(session.getId().toUpperCase(),             session);
     }
 
+    
+    
     @OnClose
-    public void onClose(Session session) {
-        
+    public void onClose(Session session)
+    {
+
         users.remove(session.getId().toUpperCase());
-       
-        
+
     }
 
     @OnError
-    public void onError(Throwable t) 
+    public void onError(Throwable t)
     {
 
     }
 
+    
+    
+    /*
+     OnMessage - обработка события получения сервером сообщения от клиента
+    */
     @OnMessage
-    public void onMessage(String message, Session session) 
+    //                         сообщение       экземпляр класса сессии с клиентом
+    //                             |           /
+    public void onMessage(String message, Session session)
     {
-        try
-        {
-       
-            session.getBasicRemote().sendText(message);
-       
-        } catch (IOException ex)
-        {
-       
-        }
+        LOG.log(Level.INFO,"Получено сообщение от клиента: " + message);
     }
     
 }
