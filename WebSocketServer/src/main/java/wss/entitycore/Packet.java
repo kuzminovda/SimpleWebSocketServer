1e5a57677fbd60bb1797aef2010e439993f930cb
kuzminovda <104501406+kuzminovda@users.noreply.github.com>
2025-09-02 07:22:27 +1000

    Update Packet.java

--- WebSocketServer/src/main/java/wss/entitycore/Packet.java
+++ WebSocketServer/src/main/java/wss/entitycore/Packet.java
@@ -1,6 +1,60 @@
 package wss.entitycore;
 
 
-public class Packet {
+public class Packet
+{
+
+   
+    private String command; // Поле команды
+    private String type;    // Поле типа данных 
+    private String body;    // Контейнер для размещения класса
     
+    
+     /**
+     * @return the command
+     */
+    public String getCommand()
+    {
+        return command;
+    }
+
+    /**
+     * @param command the command to set
+     */
+    public void setCommand(String command)
+    {
+        this.command = command;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType()
+    {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+    /**
+     * @return the body
+     */
+    public String getBody()
+    {
+        return body;
+    }
+
+    /**
+     * @param body the body to set
+     */
+    public void setBody(String body)
+    {
+        this.body = body;
+    }
 }
